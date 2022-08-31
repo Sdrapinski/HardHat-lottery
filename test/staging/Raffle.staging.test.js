67306908f4936fe99d9df6f7204246ed53350492
Sdrapinski <szymondrapinski@gmail.com>
2022-08-31 23:12:36 +0200

    staging test

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -17,6 +17,37 @@ developmentChains.includes(network.name)
         raffleEntranceFee = await raffle.getEntranceFee();
       });
       describe("fufufu", function() {
-        it("works live chainlink keeprs");
+        it("works live chainlink keepers and chainlink vrf and get a random winner", async function() {
+          const startingTimeStamp = await raffle.getLatestTimeStamp();
+          const accounts = await ethers.getSigner();
+
+          await new Promise(async (resolve, reject) => {
+            raffle.once("WinnerPicked", async () => {
+              console.log("WinnerPicked");
+
+              try {
+                const recentWinner = await raffle.getRecentWinner();
+                const raffleState = await raffle.getRafflestate();
+                const winnerBalance = await accounts[0].getBalance();
+                const endingTimeStamp = await raffle.getLatestTimeStamp();
+
+                await expect(raffle.getPlayer(0)).to.be.reverted;
+                assert.equal(recentWinner.toString(), accounts[0].address);
+                assert.equal(rafflestate, 0);
+                assert.equal(
+                  winnerEndingBalance.toString(),
+                  winnerStartingBalance.add(raffleEntranceFee.toString())
+                );
+                assert(endingTimeStamp > startingTimeStamp);
+                resolve();
+              } catch (error) {
+                console.log(error);
+                reject(e);
+              }
+            });
+            await raffle.enterRaffle({ value: raffleEntranceFee });
+            const winnerStartingBalance = await accounts[0].getBalance();
+          });
+        });
       });
     });
