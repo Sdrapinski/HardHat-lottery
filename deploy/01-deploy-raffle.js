0e644930f6ae075c9501b0d9e17c6e2c8fc8d32e
Sdrapinski <szymondrapinski@gmail.com>
2022-09-01 01:53:09 +0200

    done

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,65 +1,80 @@
-const { network, ethers } = require("hardhat");
 const {
-  developmentChains,
+  getNamedAccounts,
+  deployments,
+  network,
+  run,
+  ethers,
+} = require("hardhat");
+const {
   networkConfig,
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
 } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
-const { verify } = require("../helper-hardhat-config");
+const FUND_AMOUNT = "1000000000000000000000";
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("3");
-
-module.exports = async function({ getNamedAccounts, deployments }) {
+module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
-  let vrfCoordinatorV2Address, subscriptionId;
+  let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock;
 
-  if (developmentChains.includes(network.name)) {
-    const vrfCoordinatorV2Mock = await ethers.getContract(
-      "VRFCoordinatorV2Mock"
-    );
+  if (chainId == 31337) {
+    // create VRFV2 Subscription
+    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
     const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
-    const transactionReceipt = await transactionResponse.wait(1);
+    const transactionReceipt = await transactionResponse.wait();
     subscriptionId = transactionReceipt.events[0].args.subId;
-
-    await vrfCoordinatorV2Mock.fundSubscription(
-      subscriptionId,
-      VRF_SUB_FUND_AMOUNT
-    );
+    // Fund the subscription
+    // Our mock makes it so we don't actually have to worry about sending fund
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
   } else {
-    vrfCoordinatorV2Address = networkConfig[chainId]["VRFCoordinatorV2"];
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
     subscriptionId = networkConfig[chainId]["subscriptionId"];
   }
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
 
-  const entranceFee = networkConfig[chainId]["entranceFee"];
-  const gasLane = networkConfig[chainId]["gasLane"];
-  const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
-  const interval = networkConfig[chainId]["interval"];
-
-  const args = [
+  log("----------------------------------------------------");
+  const arguments = [
     vrfCoordinatorV2Address,
-    entranceFee,
-    gasLane,
     subscriptionId,
-    callbackGasLimit,
-    interval,
+    networkConfig[chainId]["gasLane"],
+    networkConfig[chainId]["interval"],
+    networkConfig[chainId]["entranceFee"],
+    networkConfig[chainId]["callbackGasLimit"],
   ];
   const raffle = await deploy("Raffle", {
     from: deployer,
-    args: args,
+    args: arguments,
     log: true,
-    waitConfirmations: network.config.blockConfirmations || 1,
+    waitConfirmations: waitBlockConfirmations,
   });
 
+  // Programmatically adding a consumer for the vrfCoordinatorV2Mock
+  if (developmentChains.includes(network.name)) {
+    await vrfCoordinatorV2Mock.addConsumer(
+      subscriptionId.toNumber(),
+      raffle.address
+    );
+  }
+
+  // Verify the deployment
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
-    log("Veryfying");
-    await verify(raffle.address, args);
+    log("Verifying...");
+    await verify(raffle.address, arguments);
   }
-  log("------------");
+
+  log("Enter lottery with command:");
+  const networkName = network.name == "hardhat" ? "localhost" : network.name;
+  log(`yarn hardhat run scripts/enterRaffle.js --network ${networkName}`);
+  log("----------------------------------------------------");
 };
 
 module.exports.tags = ["all", "raffle"];
--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -4,49 +4,60 @@ const {
   developmentChains,
   networkConfig,
 } = require("../../helper-hardhat-config");
-//
 
 developmentChains.includes(network.name)
   ? describe.skip
-  : describe(" Raffle Unit Tests ", function() {
+  : describe("Raffle Staging Tests", function() {
       let raffle, raffleEntranceFee, deployer;
 
       beforeEach(async function() {
         deployer = (await getNamedAccounts()).deployer;
-        raffle = await ethers.getContract(" Raffle ", deployer);
+        raffle = await ethers.getContract("Raffle", deployer);
         raffleEntranceFee = await raffle.getEntranceFee();
       });
-      describe("fufufu", function() {
-        it("works live chainlink keepers and chainlink vrf and get a random winner", async function() {
-          const startingTimeStamp = await raffle.getLatestTimeStamp();
-          const accounts = await ethers.getSigner();
 
+      describe("fulfillRandomWords", function() {
+        it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function() {
+          // enter the raffle
+          console.log("Setting up test...");
+          const startingTimeStamp = await raffle.getLastTimeStamp();
+          const accounts = await ethers.getSigners();
+
+          console.log("Setting up Listener...");
           await new Promise(async (resolve, reject) => {
+            // setup listener before we enter the raffle
+            // Just in case the blockchain moves REALLY fast
             raffle.once("WinnerPicked", async () => {
-              console.log("WinnerPicked");
-
+              console.log("WinnerPicked event fired!");
               try {
+                // add our asserts here
                 const recentWinner = await raffle.getRecentWinner();
-                const raffleState = await raffle.getRafflestate();
-                const winnerBalance = await accounts[0].getBalance();
-                const endingTimeStamp = await raffle.getLatestTimeStamp();
+                const raffleState = await raffle.getRaffleState();
+                const winnerEndingBalance = await accounts[0].getBalance();
+                const endingTimeStamp = await raffle.getLastTimeStamp();
 
                 await expect(raffle.getPlayer(0)).to.be.reverted;
                 assert.equal(recentWinner.toString(), accounts[0].address);
-                assert.equal(rafflestate, 0);
+                assert.equal(raffleState, 0);
                 assert.equal(
                   winnerEndingBalance.toString(),
-                  winnerStartingBalance.add(raffleEntranceFee.toString())
+                  winnerStartingBalance.add(raffleEntranceFee).toString()
                 );
                 assert(endingTimeStamp > startingTimeStamp);
                 resolve();
               } catch (error) {
                 console.log(error);
-                reject(e);
+                reject(error);
               }
             });
-            await raffle.enterRaffle({ value: raffleEntranceFee });
+            // Then entering the raffle
+            console.log("Entering Raffle...");
+            const tx = await raffle.enterRaffle({ value: raffleEntranceFee });
+            await tx.wait(1);
+            console.log("Ok, time to wait...");
             const winnerStartingBalance = await accounts[0].getBalance();
+
+            // and this code WONT complete until our listener has finished listening!
           });
         });
       });
